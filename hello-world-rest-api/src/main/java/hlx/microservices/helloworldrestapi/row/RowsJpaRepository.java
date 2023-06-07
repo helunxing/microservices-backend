1c45498bff0cce52ff36ad8d141e2d84760be8f2
lx <37706103+helunxing@users.noreply.github.com>
2023-06-07 18:45:16 +0100

    Delete demo packages

--- hello-world-rest-api/src/main/java/hlx/microservices/helloworldrestapi/row/RowsJpaRepository.java
+++
@@ -1,6 +0,0 @@
-package hlx.microservices.helloworldrestapi.row;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RowsJpaRepository extends JpaRepository <Rows, Long>{
-}
