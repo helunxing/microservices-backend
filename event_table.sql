e71deb5422c8cc2ac0e9cae13dde6c9c748faec4
lx <37706103+helunxing@users.noreply.github.com>
2023-07-15 23:12:35 +0100

    Create event_table.sql

---
+++ event_table.sql
@@ -0,0 +1,24 @@
+INSERT INTO public.user_table (id, sub, login_key) VALUES (1, 'github_37706103', 'default_key');
+INSERT INTO public.user_table (id, sub, login_key) VALUES (2, 'undefined', 'default_key');
+
+INSERT INTO public.event_table (id, creator_id, title, address, date, time_options) VALUES (1, 2, 'lx''s birthday', e'Kelvin Court
+30 Yorkhill Street
+GXXXXX', '2023-06-27', '17:02_17:32,17:03_17:33,17:03_17:33');
+INSERT INTO public.event_table (id, creator_id, title, address, date, time_options) VALUES (3, 2, 'Graudate party', e'University Of Glasgow1
+Gilmorehill
+GXXXXX', '2023-06-27', '19:30_23:00,18:30_23:00,20:30_23:00');
+INSERT INTO public.event_table (id, creator_id, title, address, date, time_options) VALUES (4, 2, 'Meeting for class work', e'107 Kelvinhaugh Street
+Flat 24
+GXXXXX', '2023-06-27', '17:57_18:27,17:57_18:27,17:57_18:27');
+INSERT INTO public.event_table (id, creator_id, title, address, date, time_options) VALUES (5, 2, 'Picnic in park', e'Flat 23
+107 Kelvinhaugh Street
+GXXXXX', '2023-06-27', '19:30_23:00,18:30_23:00,20:30_23:00');
+INSERT INTO public.event_table (id, creator_id, title, address, date, time_options) VALUES (8, 2, 'Museum tour', e'Kelvin Court
+30 Yorkhill Street
+GXXXXX', '2023-06-27', '19:30_23:00,18:30_23:00,20:30_23:00');
+INSERT INTO public.event_table (id, creator_id, title, address, date, time_options) VALUES (10, 2, 'Scott and his student''s group meeting', e'Flat 22
+107 Kelvinhaugh Street
+GXXXXX', '2023-06-27', '19:30_23:00,18:30_23:00,20:30_23:00');
+INSERT INTO public.event_table (id, creator_id, title, address, date, time_options) VALUES (11, 1, 'Look at rental properties on the spot', e'107 Kelvinhaugh Street
+Flat 24
+GXXXXX', '2023-07-08', '17:02_17:32,17:03_17:33,17:03_17:33');
