d3b5dbf6dafaf5d59706a60a75c54196e0d9a3cf
lx <37706103+helunxing@users.noreply.github.com>
2023-07-22 21:16:47 +0100

    Change Responses Notfound entity

--- event/src/main/java/timeag/backend/event/Responses.java
+++ event/src/main/java/timeag/backend/event/Responses.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
 public abstract class Responses {
-    public static ResponseEntity<Object> NotFoundEntity() {
+    public static ResponseEntity<Object> NotFoundEntity(String message) {
         // BFF layer only read the status code, "not found" is just a reminder for user.
-        return new ResponseEntity<>("Not Found", HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(message, HttpStatus.NOT_FOUND);
     }
 
 }
